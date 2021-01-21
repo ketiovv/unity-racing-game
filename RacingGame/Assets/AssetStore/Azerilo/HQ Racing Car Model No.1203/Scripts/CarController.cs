cdb52e237e131e0a8368ab6fc298ff6fa9096a11
Wojtek <wojtek.kajstura@gmail.com>
2021-01-21 22:00:32 +0100

    re-adding project files

---
+++ RacingGame/Assets/AssetStore/Azerilo/HQ Racing Car Model No.1203/Scripts/CarController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(InputManager))]
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    public InputManager im;
+    public List<WheelCollider> throttleWheels;
+    public List<GameObject> steeringWheels;
+    public List<GameObject> meshes;
+    public float strenghtCoefficent = 10000f;
+    public float maxTurn = 20f;
+    public Transform CM;
+    public Rigidbody rb;
+    void Start()
+    {
+        im = GetComponent<InputManager>();
+        rb = GetComponent<Rigidbody>();
+        if(CM)
+        {
+            rb.centerOfMass = CM.localPosition;
+        }
+    }
+    void FixedUpdate()
+    {
+        foreach (var wheel in throttleWheels)
+        {
+            wheel.motorTorque = strenghtCoefficent * Time.deltaTime * im.throttle; 
+        }
+
+        foreach (var wheel in steeringWheels)
+        {
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
+            wheel.transform.localEulerAngles = new Vector3(0f, im.steer * maxTurn ,0f);
+        }
+
+        foreach (var mesh in meshes)
+        {
+            mesh.transform.Rotate(rb.velocity.magnitude * (transform.InverseTransformDirection(rb.velocity).z >=0 ? 1: -1) / (2 * Mathf.PI * 0.27f), 0f, 0f);
+        }
+    }
+}
---
+++ RacingGame/Assets/Scripts/FinishCheckpointTrigger.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinishCheckpointTrigger : MonoBehaviour
+{
+    public GameObject FinishCheckpointTrig;
+    public GameObject Sector2CheckpointTrig;
+
+    public GameObject LastLapMiliSeconds;
+    public GameObject LastLapSeconds;
+    public GameObject LastLapMinutes;
+
+    public GameObject BestLapMiliSeconds;
+    public GameObject BestLapSeconds;
+    public GameObject BestLapMinutes;
+
+    public GameObject LapTimeBox;
+
+    void OnTriggerEnter()
+    {
+        if (LapTimeManager.MinuteCount <= 9)
+        {
+            LastLapMinutes.GetComponent<Text>().text = "0" + LapTimeManager.MinuteCount + ":";
+        }
+        else
+        {
+            LastLapMinutes.GetComponent<Text>().text = "" + LapTimeManager.MinuteCount + ":";
+        }
+
+        if (LapTimeManager.SecondCount <= 9)
+        {
+            LastLapSeconds.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
+        }
+        else
+        {
+            LastLapSeconds.GetComponent<Text>().text = "" + LapTimeManager.SecondCount + ".";
+        }
+
+        LastLapMiliSeconds.GetComponent<Text>().text = "" + LapTimeManager.MiliSecondCount;
+
+
+        var bestLapMinutes = int.Parse(BestLapMinutes.GetComponent<Text>().text.Trim(':', '.'));
+        var bestLapSeconds = int.Parse(BestLapSeconds.GetComponent<Text>().text.Trim(':', '.'));
+        //when milisecond is 9.6352 we get only 9
+        var bestLapMiliSeconds = int.Parse(BestLapMiliSeconds.GetComponent<Text>().text.Substring(0, 1));
+
+
+        if (bestLapMinutes == 0 && bestLapSeconds == 0 && bestLapMiliSeconds == 0)
+        {
+            UpdateBestLapTime();
+        }
+        else
+        {
+            if (LapTimeManager.MinuteCount < bestLapMinutes)
+            {
+                UpdateBestLapTime();
+            }
+            else if (LapTimeManager.MinuteCount == bestLapMinutes)
+            {
+                if (LapTimeManager.SecondCount < bestLapSeconds)
+                {
+                    UpdateBestLapTime();
+                }
+                else if (LapTimeManager.SecondCount == bestLapSeconds)
+                {
+                    if (LapTimeManager.MiliSecondCount < bestLapMiliSeconds)
+                    {
+                        UpdateBestLapTime();
+                    }
+                }
+
+            }
+        }
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MiliSecondCount = 0;
+
+        FinishCheckpointTrig.SetActive(false);
+        Sector2CheckpointTrig.SetActive(true);
+    }
+
+    void UpdateBestLapTime()
+    {
+        if (LapTimeManager.MinuteCount <= 9)
+        {
+            BestLapMinutes.GetComponent<Text>().text = "0" + LapTimeManager.MinuteCount + ":";
+        }
+        else
+        {
+            BestLapMinutes.GetComponent<Text>().text = "" + LapTimeManager.MinuteCount + ":";
+        }
+
+        if (LapTimeManager.SecondCount <= 9)
+        {
+            BestLapSeconds.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
+        }
+        else
+        {
+            BestLapSeconds.GetComponent<Text>().text = "" + LapTimeManager.SecondCount + ".";
+        }
+
+        BestLapMiliSeconds.GetComponent<Text>().text = "" + LapTimeManager.MiliSecondCount;
+    }
+}
