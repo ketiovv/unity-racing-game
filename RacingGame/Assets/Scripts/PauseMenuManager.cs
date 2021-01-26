3ce3252e33b7d7b22f8bdfe79ccc2ec8ca47d980
Wojtek <wojtek.kajstura@gmail.com>
2021-01-26 16:01:48 +0100

    working on pause menu

---
+++ RacingGame/Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUi;
+
+    [SerializeField] private bool isPaused;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseMenuUi.SetActive(true);
+    }
+
+    void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseMenuUi.SetActive(false);
+    }
+}
